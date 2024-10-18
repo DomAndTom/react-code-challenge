4ea732cf3376468afd4fcfa833574f355097ed2f
c-kehler <charlespkehler@gmail.com>
2024-10-18 13:47:10 -0400

    resolve dependency errors

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -5,7 +5,7 @@ import Wrapper from "../Wrapper";
 import { useSearchContext } from "../../contexts/search";
 
 import { searchRamone } from "../../helpers/search";
-import { list } from "../../list.json";
+import list from "../../list.json";
 
 export default function Content() {
   const [ramones, setRamones] = useState([]);
